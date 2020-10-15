31e91873a27a4c24501d560ffc3793fc0af6c397
dherliansyah <dikiherliansyahh@gmail.com>
2020-10-15 14:09:26 +0700

    commit

--- Helper/Middleware.js
+++ Helper/Middleware.js
@@ -2,13 +2,14 @@ const jwt = require("jsonwebtoken");
 
 const middlewareVerify = (req, res, next) => {
   const bearearToken = req.header("auth-token");
-  const token = bearearToken.split(" ")[1];
-  if (!token) {
+  
+  if (!bearearToken) {
     res.status(401).send({
       success: false,
       message: "NOT FOUND",
     });
   } else {
+    const token = bearearToken.split(" ")[1];
     jwt.verify(token, process.env.DATA_KEY, (err, decode) => {
       if (!err) {
         if (decode.role == "20") next();
